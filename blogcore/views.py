93014f79126f546118a834fbd07f96fe5b1d599d
Metleb1996 <metlebrustemov1996@gmail.com>
2023-03-27 16:48:32 +0400

    moved

---
+++ blogcore/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html', context={})
+
+def about(request):
+    return render(request, 'about.html', context={})
+
+def post(request):
+    return render(request, 'post.html', context={})
+
+
+def contact(request):
+    return render(request, 'contact.html', context={})
