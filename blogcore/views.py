77c8f7c3d07f6406e84ef48d52c64e677e0f5059
Metleb1996 <metlebrustemov1996@gmail.com>
2023-04-07 11:21:39 +0400

    footer component

--- blogcore/views.py
+++ blogcore/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 
+from blogcore.models import BlogPost
+
 def index(request):
-    return render(request, 'index.html', context={})
+    posts = BlogPost.objects.all()[:10]  
+    return render(request, 'index.html', context={"posts":posts})
 
 def about(request):
     return render(request, 'about.html', context={})
 
 def post(request):
     return render(request, 'post.html', context={})
+
+def posts(request):
+    return render(request, 'posts.html', context={})
 
 
 def contact(request):
