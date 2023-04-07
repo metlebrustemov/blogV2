e3e75d4bb30ad5b2862f94f8fd37770ab4b93e18
Metleb1996 <metlebrustemov1996@gmail.com>
2023-04-07 11:20:20 +0400

    created models

--- blogcore/models.py
+++ blogcore/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.utils.text import slugify
+from django.utils import timezone
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+class Tag(models.Model):
+    title = models.CharField(max_length=64,default="")
+    slug = models.CharField(max_length=96,default="")
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        instance = super(Tag, self).save(*args, **kwargs)
+        
+        return instance
+    
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=128,default="")
+    subtitle = models.CharField(max_length=256,default="")
+    description = models.CharField(max_length=512,default="")
+    body = RichTextField(default="")
+    pub_date = models.DateTimeField('date published', default=timezone.now)
+    is_pin = models.BooleanField(default=False, verbose_name="Is Pin")
+    is_published = models.BooleanField(default=False, verbose_name="Is Publised")
+    slug = models.SlugField(max_length=200, unique=True, verbose_name="Slug")
+    tags = models.ManyToManyField(Tag, blank=True)
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        instance = super(BlogPost, self).save(*args, **kwargs)
+        
+        return instance
+
+class Setting(models.Model):
+    title = models.CharField(max_length=128,default="")
+    subtitle = models.CharField(max_length=256,default="")
+    about = RichTextField(default="")
+    def __str__(self):
+        return self.title
+    
+   
+
+
+
