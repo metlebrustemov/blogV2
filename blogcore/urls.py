737bbd13b9db9314d48e3e56307858b133d017f0
Metleb1996 <metlebrustemov1996@gmail.com>
2023-04-07 11:20:42 +0400

    added posts url

--- blogcore/urls.py
+++ blogcore/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
+    path('posts', views.posts, name='posts'),
     path('contact', views.contact, name='contact'),
 ]
