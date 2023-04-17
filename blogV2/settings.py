f215e73d80de58e4e361a2a1e4e3efc5d4edcabb
Metleb1996 <metlebrustemov1996@gmail.com>
2023-04-17 11:43:55 +0400

    jazzmin

--- blogV2/settings.py
+++ blogV2/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qfl8v#^mle$+b1vo%s754_4e=%lfq$usq@+(!3z)xv$_hn3%$y'  if os.environ.get('SECRET_KEY') is None  else os.environ['SECRET_KEY'] 
+SECRET_KEY = 'django-insecure-qfl8v#^mle$+b1vo%s754_4e=%lfq$usq@+(!3z)xv$_hn3%$y' if os.environ.get(
+    'SECRET_KEY') is None else os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,6 +33,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -144,5 +146,70 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CKEDITOR_CONFIGS = {
     'default': {
         'toolbar': 'full',
+    },
+}
+
+JAZZMIN_SETTINGS = {
+    "site_title": "Lecum Admin",
+    "site_header": "Lecum",
+    "site_brand": "Lecum",
+    "copyright": "Metleb Rustemov",
+    "user_avatar": None,
+     "show_sidebar": True,
+    "navigation_expanded": True,
+    "topmenu_links": [
+
+        {"name": "Home",  "url": "admin:index",
+            "permissions": ["auth.view_user"]},
+
+
+        {"model": "auth.User"},
+
+        {"app": "blogcore"},
+    ],
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+        "auth.Group": "fas fa-users",
+        "blogcore.Tag": "fas  fa-tag",
+        "blogcore.BlogPost": "fas fa-pen",
+        "blogcore.Setting": "fas fa-wrench",
+    },
+
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-circle",
+    "changeform_format_overrides": {
+        "auth.user": "collapsible",
+        "auth.group": "vertical_tabs",
+    },
+}
+JAZZMIN_UI_TWEAKS = {
+      "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "accent": "accent-teal",
+    "navbar": "navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-info",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "cyborg",
+    "dark_mode_theme": "cyborg",
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success",
     },
 }
