d94189e8a7e96b43fec3970e2ffda55280997c4e
Metleb1996 <metlebrustemov1996@gmail.com>
2023-05-10 17:14:02 +0400

    user admin

--- blogcore/admin.py
+++ blogcore/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from blogcore.forms import AdLucemUserChangeForm, AdLucemUserCreationForm
 
-from .models import BlogPost, Tag, Setting
+from blogcore.models import AdLucemUser, BlogPost, Tag, Setting
+
+class AdLucemUserAdmin(UserAdmin):
+    add_form = AdLucemUserCreationForm
+    form = AdLucemUserChangeForm
+    model = AdLucemUser
+    list_display = ("email", "is_staff", "is_active",)
+    list_filter = ("email", "is_staff", "is_active",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active", "groups", "user_permissions"
+            )}
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
+
+admin.site.register(AdLucemUser, AdLucemUserAdmin)
 
 @admin.register(BlogPost)
 class AdminBlogPost(admin.ModelAdmin):
